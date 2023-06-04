ff50081df50be8bbeed9c516384c041bdc78d14d
Venkata Matcha <mvs.teja91@gmail.com>
2023-06-04 11:05:54 -0400

    gh build config

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/dog-home-finder-app/',
   plugins: [
     react({
       jsxImportSource: '@welldone-software/why-did-you-render',
