5316cbc4a0237a7f953c79ecde777f743a727559
Venkata Matcha <mvs.teja91@gmail.com>
2023-06-05 23:08:00 -0400

    route configs

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,7 @@
 export const PUBLIC_PATH = '/dog-home-finder-app';
 export const ROUTE_CODES = {
-  HOME: PUBLIC_PATH,
-  SIGNIN: `${PUBLIC_PATH}/signin`,
+  HOME: `${PUBLIC_PATH}/home`,
+  SIGNIN: PUBLIC_PATH,
 };
 
 export const COLOR_SCHEME_QUERY = '(prefers-color-scheme: dark)';
